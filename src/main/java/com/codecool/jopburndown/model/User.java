17008be4e351523118b7431e55c741af5081e99d
szkrecsi <ecsenszki.judit@gmail.com>
2017-05-31 16:27:12 +0200

    User class is added with hibernate annotations

--- src/main/java/com/codecool/jopburndown/model/User.java
+++ src/main/java/com/codecool/jopburndown/model/User.java
@@ -1,5 +1,20 @@
 package com.codecool.jopburndown.model;
 
+import javax.persistence.*;
 
+@Entity
+@Table(name = "user")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private int id;
+    private String username;
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 }
