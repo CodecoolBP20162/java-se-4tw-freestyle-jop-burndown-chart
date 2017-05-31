852253c10a41bcf6a50b1b38155a7fae2af3dcd3
szkrecsi <ecsenszki.judit@gmail.com>
2017-05-31 16:35:35 +0200

    buildTablesWithUniqueConnection method is added

--- src/main/java/com/codecool/jopburndown/Main.java
+++ src/main/java/com/codecool/jopburndown/Main.java
@@ -1,14 +1,22 @@
+package com.codecool.jopburndown;
+
 import com.codecool.jopburndown.controller.FieldController;
 import com.codecool.jopburndown.controller.MainController;
 import com.codecool.jopburndown.database.DbHandler;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import spark.template.thymeleaf.ThymeleafTemplateEngine;
 import static spark.Spark.*;
 import com.codecool.jopburndown.model.Board;
 
 public class Main {
+
     public static void main(String[] args) {
-        //DbHandler x = new DbHandler();
-        DbHandler.configure();
+
+        DbHandler.buildTablesWithUniqueConnection();
+
+        SessionFactory sessionFactory = DbHandler.getSessionFactory();
+        Session session = sessionFactory.openSession();
 
         exception(Exception.class, (e, req, res) -> e.printStackTrace());
         staticFileLocation("/public");
